71ddf017637d06cd28bbcd56d91bca6fb38443e3
José Ramón Navarro <jrng99@gmail.com>
2023-07-09 21:06:25 +0200

    Generación del par de claves SSH en terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,3 +10,12 @@ output "sg_id" {
   value = azurerm_network_security_group.sg.id
 }
 
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.vm.public_ip_address
+}
+
+output "tls_private_key" {
+  value = tls_private_key.claves_ssh.private_key_pem
+  sensitive = true
+}
+
--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,8 +1,10 @@
+# Creacion del resource_group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location_name
 }
 
+# Creacaión de la red virtual
 resource "azurerm_virtual_network" "vnet" {
   name                = var.network_name
   address_space       = ["10.0.0.0/16"]
@@ -10,6 +12,7 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+# Creacion de la subred
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
@@ -17,6 +20,7 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+# Solicitando una Ip pública
 resource "azurerm_public_ip" "pip" {
   name                = "VIP"
   location            = azurerm_resource_group.rg.location
@@ -26,33 +30,35 @@ resource "azurerm_public_ip" "pip" {
   domain_name_label   = "domaintest"  
 }
 
+# Creando el security group
 resource "azurerm_network_security_group" "sg"{
   name                = "vsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "reglapuerto80"
-    priority                   = 1000
+    name                       = "SSH"
+    priority                   = 1001
     direction                  ="Inbound"
     access                     ="Allow"
     protocol                   ="Tcp"
     source_port_range          ="*"
-    destination_port_range     ="80"
+    destination_port_range     ="22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
-
   }
   tags={
     environment ="Test"
   }
 }
 
+# Asociando a la subred el security group
 resource "azurerm_subnet_network_security_group_association" "ngs-link" {
   subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.sg.id
 }
 
+# Creando el interface de red y asignandole la subred y la ip pública
 resource "azurerm_network_interface" "nic" {
   name                = "vnic"
   location            = azurerm_resource_group.rg.location
@@ -67,11 +73,19 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+# Creando y mostrando la clave SSH
+resource "tls_private_key" "claves_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Asignando al interface de red el security group
 resource "azurerm_network_interface_security_group_association" "nicgs"{
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.sg.id
 }
 
+# Creando la maquina virtual 
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "vm1"
   resource_group_name = azurerm_resource_group.rg.name
@@ -85,7 +99,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = tls_private_key.claves_ssh.public_key_openssh
+    #public_key = file("~/.ssh/id_rsa.pub")
   }
 
   os_disk {
